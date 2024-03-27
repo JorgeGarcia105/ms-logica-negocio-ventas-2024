40b6ded546d07cca22121738477c92af5dfa12b3
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-03-27 13:55:12 -0500

    Integracion de estrategias de autorizacion 1-2-3-4

---
+++ src/config/auth/strategy.ts
@@ -0,0 +1,59 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {AuthenticationBindings} from '@loopback/authentication/dist/keys';
+import {AuthenticationMetadata} from '@loopback/authentication/dist/types';
+import {inject} from '@loopback/context';
+import {HttpErrors, Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+import parseBearerToken from 'parse-bearer-token';
+import {ConfiguracionSeguridad} from '../configuracion.seguridad';
+const fetch = require('node-fetch');
+
+export class AuthStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
+
+  constructor(
+    @inject(AuthenticationBindings.METADATA)
+    private metadata: AuthenticationMetadata[],
+  ) { }
+
+
+  /**
+   * Autentificacion de un usuario frente a la base de datos
+   * @parc:\Users\garci\proyecto-ventas\backend\ms-seguridad\src\autham request la solicitud con el token de autenticación
+   * @returns el perfil del usuario, undefined cuando no tiene permisos o un HttpErrors[401] cuando no tiene token
+   */
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+    let token = parseBearerToken(request);
+    if (token) {
+      let idMenu: string = this.metadata[0].options![0];
+      let accion: string = this.metadata[0].options![1];
+      console.log(this.metadata)
+
+      const datos = {token, idMenu: idMenu, accion: accion};
+      const urlValidarPermisos = `${ConfiguracionSeguridad.enlaceMicroservicioSeguridad}/validar-permisos`;
+      let res = undefined;
+
+      try {
+        await fetch(urlValidarPermisos, {
+          method: 'post',
+          datos: JSON.stringify(datos),
+          headers: {'Content-Type': 'application/json'},
+        }).then((res: any) => res.json())
+          .then((json: any) => {
+            res = json;
+          });
+        if (res) {
+          let perfil: UserProfile = Object.assign({
+            permitido: "OK"
+          });
+          return perfil;
+        } else {
+          return undefined;
+        }
+      } catch (e) {
+        throw new HttpErrors[401]('No se tiene permisos sobre la accion a ejecutar.');
+      }
+    }
+    throw new HttpErrors[401]('No es posible ejecutar la acción por falta de un token de autenticación.');
+  }
+}
---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,9 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "660211163bf10b0b28f324e9";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+  export const enlaceMicroservicioSeguridad = "http://localhost:3001";
+}
