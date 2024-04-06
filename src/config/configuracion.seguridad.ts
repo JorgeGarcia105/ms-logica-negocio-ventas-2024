7b27809990d8be60c5253b522cb6fb8d51a9feab
Jorge Ivan Garcia Torres <111246512+JorgeGarcia105@users.noreply.github.com>
2024-04-06 18:18:20 -0500

    Paginacion de registros. Interceptors en Angular

--- src/config/configuracion.seguridad.ts
+++ src/config/configuracion.seguridad.ts
@@ -1,5 +1,6 @@
 export namespace ConfiguracionSeguridad {
   export const menuProductoId = "660211163bf10b0b28f324e9";
+  export const menuClienteId = "661169b9cf254bfc277ffbae";
   export const listarAccion = "listar";
   export const guardarAccion = "guardar";
   export const editarAccion = "editar";
